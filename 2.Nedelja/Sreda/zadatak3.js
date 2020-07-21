07c4e76e2111f99a3d690bf7f0326296bfbd919c
srdjansolajic <solajic.srdjan@gmail.com>
2020-07-21 08:44:15 +0200

    Dodati zadatak 3 i 4

---
+++ 2.Nedelja/Sreda/zadatak3.js
@@ -0,0 +1,7 @@
+let num = 40;
+
+if (num % 3 == 0) {
+    console.log('Broj', num, 'je deljiv sa 3');
+} else {
+    console.log('Broj', num, 'nije deljiv sa 3');
+}
---
+++ 2.Nedelja/Sreda/zadatak4.js
@@ -0,0 +1,6 @@
+for (let i = 0; i <= 1000; i++) {
+    if (i % 5 == 0) {
+        console.log(i);
+    }    
+}
+
